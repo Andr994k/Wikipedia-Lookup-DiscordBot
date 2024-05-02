50c13f66b3c626b27f8df6e8b086aec400647e9c
GedeGustav <gustavsvare@gmail.com>
2024-05-02 13:15:46 +0200

    Update list scraper.py

--- list scraper.py
+++ list scraper.py
@@ -1,46 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://wikis.fandom.com/wiki/Category:Games_hub"
+def Get_List(URL: str):
+    LIST = []
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    wikitable = soup.find(class_="mw-parser-output")
 
-page = requests.get(URL)
+    data = wikitable.find_all("tr", limit=6)
+    data = data[5]
+    data = str(data)
 
-soup = BeautifulSoup(page.content, "html.parser")
+    titleIndex = [i for i in range(len(data)) if data.startswith(" title=", i)] 
+    wikiIndex = [i for i in range(len(data)) if data.startswith(r'">', i)] 
 
-encoded_html = soup.prettify("utf-8")
+    for x in titleIndex:
+        name = data[x + 7: wikiIndex[titleIndex.index(x)]]
+        name = name.replace('"', "")
+        name = name.replace(" Wiki", "")
 
-wikitable = soup.find(class_="mw-parser-output")
+        LIST.append(name)
 
+    return LIST
 
-data = wikitable.find_all("tr", limit=6)
-data = data[5]
-data = str(data)
-
-#print(data)
-
-titleIndex = [i for i in range(len(data)) if data.startswith(" title=", i)] 
-#print(titleIndex)
-
-wikiIndex = [i for i in range(len(data)) if data.startswith(r'">', i)] 
-#print(wikiIndex) Wiki"
-
-print(len(titleIndex))
-print(len(wikiIndex))
-
-games = []
-
-for x in titleIndex:
-    #print(data[x + 7: wikiIndex[titleIndex.index(x)]])
-    name = data[x + 7: wikiIndex[titleIndex.index(x)]]
-    name = name.replace('"', "")
-    name = name.replace(" Wiki", "")
-
-    games.append(name)
-
+games = Get_List("https://wikis.fandom.com/wiki/Category:Games_hub")
+shows = Get_List("https://wikis.fandom.com/wiki/Category:TV_hub")
 
 print(games)
-
-
-"""f = open("test.txt","w+")
-f.write(data)
-f.close()"""
+print(shows)
