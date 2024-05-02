75827053ff5aaa8fcaa938c1b333ad508973d535
GedeGustav <gustavsvare@gmail.com>
2024-05-02 12:52:38 +0200

    bing chilling

--- list scraper.py
+++ list scraper.py
@@ -11,16 +11,36 @@ encoded_html = soup.prettify("utf-8")
 
 wikitable = soup.find(class_="mw-parser-output")
 
+
 data = wikitable.find_all("tr", limit=6)
-
 data = data[5]
+data = str(data)
 
-print(data)
+#print(data)
+
+titleIndex = [i for i in range(len(data)) if data.startswith(" title=", i)] 
+#print(titleIndex)
+
+wikiIndex = [i for i in range(len(data)) if data.startswith(r'">', i)] 
+#print(wikiIndex) Wiki"
+
+print(len(titleIndex))
+print(len(wikiIndex))
+
+games = []
+
+for x in titleIndex:
+    #print(data[x + 7: wikiIndex[titleIndex.index(x)]])
+    name = data[x + 7: wikiIndex[titleIndex.index(x)]]
+    name = name.replace('"', "")
+    name = name.replace(" Wiki", "")
+
+    games.append(name)
 
 
+print(games)
 
-#wikitable = wikitable.prettify("utf-8")
 
-"""f = open("RAWgames.txt","w+")
-f.write(str(wikitable))
+"""f = open("test.txt","w+")
+f.write(data)
 f.close()"""
